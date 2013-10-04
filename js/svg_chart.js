002fffbe10e9e6cd9b8b5b5624668f9e6f094ea1
rnpenguin <rosshnaylor@gmail.com>
2013-10-03 21:42:34 -0700

    Updated svg chart to latest structure and keep old simulated version.

--- js/svg_chart.js
+++ js/svg_chart.js
@@ -10,15 +10,16 @@ var data = [
         "color": "#0000FF"
     }
 ];
+
 var chart;
 
 nv.addGraph(function () {
     chart = nv.models.scatterChart();
 
-    chart.forceY([40,250]);
     var now = Date.now();
 
     chart.xAxis
+        .orient("bottom")
         .tickValues([now])
         .axisLabel('Time(min)')
         .tickFormat(function (d) {
@@ -26,8 +27,18 @@ nv.addGraph(function () {
         });
 
     chart.yAxis
-        .tickValues([80, 180])
-        .axisLabel('mg/dL');
+        //.orient("right")
+        .tickValues([80, 180]);
+        //.axisLabel('mg/dL');
+
+    chart.forceY([40,300]);
+
+    //var y = d3.scale.log()
+    //chart.yScale(y)
+
+    d3.select('#chart').on("click", function () {
+        socket.emit('update', 6);
+    });
 
     nv.utils.windowResize(chart.update);
 
@@ -36,7 +47,7 @@ nv.addGraph(function () {
 
 function redraw() {
 
-    var newTime = data[0].values[data[0].values.length - 1].x;
+    var newTime = new Date();
 
     d3.select('#chart svg')
         .datum(data)
@@ -44,58 +55,31 @@ function redraw() {
         .call(chart);
 
     chart.xAxis
-        .tickValues([newTime.getTime() + 1000*60*5]);  //for some reason the tickValues reports 5 mins less than inputted
+        .tickValues([newTime.getTime()]);
 
     chart.tooltipContent(function(key, x, y) {return y + " mg/dL (" + key + ")";});
 
-    document.getElementById("currentBG").innerText = data[0].values[data[0].values.length - 1].y + " mg/dL";
-    document.getElementById("currentTime").innerText = newTime.toLocaleTimeString()
-}
-
-function getMockSGVData() {
-    var arr = [];
-    var initial = Math.floor(Math.random() * 50 + 100);
-    var theDate = new Date(new Date().getTime() - 1000*60*60*3);
-    for (var x = 0; x < 60*3/5 + 1; x++) {
-        arr.push({x: new Date(theDate.getTime()), y: initial + Math.floor(Math.random()*10) - 5});
-        theDate.setTime(theDate.getTime() + 1000 * 60 * 5);
-    }
-    return arr;
-}
-
-function getMockPredictedData() {
-    var arr = [];
-    var initial = data[0].values[data[0].values.length - 1].y;
-    var theDate = new Date(new Date().getTime() + 1000 * 60 * 5);
-    for (var x = 0; x < 60/5; x++) {
-        arr.push({x: new Date(theDate.getTime()), y: initial + Math.floor(Math.random()*10) - 5});
-        theDate.setTime(theDate.getTime() + 1000 * 60 * 5);
-    }
-    return arr;
+    $('#currentBG').text(data[0].values[data[0].values.length - 1].y + " mg/dL");
+    $('#currentTime').text(d3.time.format("%H:%M")(newTime));
 }
 
 //If io server exists, connect, else use mock data
 if(typeof(io) !== "undefined") {
-    data[0].values = getMockSGVData();
-    data[1].values = getMockPredictedData();
     var socket = io.connect();
     socket.on('connect', function () {
-        redraw();
+        socket.emit('test', "C --> S: Client Connected.");
+        console.log("Client connected.");
     });
     socket.on('sgv', function (d) {
-        var sgv = data[0].values;
-        var nextSVG = new Date(sgv[sgv.length - 1].x);
-        var nextValue = sgv[sgv.length - 1].y;
-        nextSVG.setTime(nextSVG.getTime() + 1000*60*5);
-        sgv.shift();
-        sgv.push({x:nextSVG, y: nextValue + Math.floor(Math.random()*10 - 5)});
-        var predicted = data[1].values;
-        var nextP = new Date(predicted[predicted.length - 1].x);
-        nextP.setTime(nextP.getTime() + 1000*60*5);
-        predicted.shift();
-        predicted.push({x:nextP, y: nextValue + Math.floor(Math.random()*10 - 5)});
-        redraw();
+        if(d.length > 1) {
+            data[0].values = d[0].map(function(obj) { return {x: new Date(obj.x), y: obj.y} });
+            data[1].values = d[1].map(function(obj) { return {x: new Date(obj.x), y: obj.y} });
+            redraw();
+            console.log("SGV data received.");
+            socket.emit('test', "C --> S: Client received data as: " + d);
+        }
     });
+
 } else {
     data[0].values = getMockSGVData();
     data[1].values = getMockPredictedData();
