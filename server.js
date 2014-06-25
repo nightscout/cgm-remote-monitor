3555f733c83ef90c97a95e202394a13999920d35
Brian Hanifin <code@brianhanifin.com>
2014-06-25 10:15:36 -0700

    Upgraded from node-static to Express,
    and now the site caches like a
    champ!

--- server.js
+++ server.js
@@ -54,7 +54,6 @@ function getAppCache(req) {
         'http://'+hostname+'/js/client.js',
         'http://'+hostname+'/js/dropdown.js',
         'http://'+hostname+'/favicon.ico',
-        'http://'+hostname+'/socket.io/socket.io.js',
         'http://'+hostname+'/bower_components/d3/d3.min.js',
         'http://'+hostname+'/bower_components/jquery/dist/jquery.min.js',
         'http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,300,400,600,700,800',
