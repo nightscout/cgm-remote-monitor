89e1bd50e56459b6e5a7834c80109d0124db85d1
m2oore <m2oore@gmail.com>
2014-07-22 19:46:02 -0700

    Update client.js

--- static/js/client.js
+++ static/js/client.js
@@ -628,8 +628,8 @@
         }
     });
     
-    socket.on('settings', function (d){
-        if (d.lenght > 1) {
+    socket.on('settings', function (d) {
+        if (d.length > 1) {
             if (d[0].length){
                 var settings = d[0]d[0].length -1];
              if (current.battery){
