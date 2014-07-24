6c99055ed17a62938118ee1ac1938cbd15614cad
Brian Hanifin <code@brianhanifin.com>
2014-07-23 17:06:54 -0700

    Toolbar hide/show buttons + Custom glyph font
    * I added toolbar hide/show functionality for users that want that UI
    out of the way.
    * I built a custom glyph font using
    [fontello.com](http://fontello.com/). These fonts are replacing images
    I was using in the toolbar. See http://localhost:1337/fonts/demo.html
    for the list of available fonts.
    * I combined drawer.js and settings.js.
    * Various other cleanup.

--- static/js/ui-utils.js
+++ static/js/ui-utils.js
@@ -1,3 +1,64 @@
+var drawerIsOpen = false;
+
+function openDrawer()  {
+	$("#container").animate({marginLeft: "-200px"}, 300);
+	$("#drawer").css("display", "block");
+	$("#drawer").animate({right: "0"}, 300);
+	drawerIsOpen = true;
+}
+function closeDrawer(callback) {
+	$("#container").animate({marginLeft: "0px"}, 300, callback);
+	$("#drawer").animate({right: "-200px"}, 300, function() {
+		$("#drawer").css("display", "none");
+	});
+	drawerIsOpen = false;
+}
+
+function openToolbar() {
+	$("#showToolbar").fadeOut(200, function() {
+		$("#toolbar").animate({marginTop: "-0px"}, 200);
+	});
+}
+function closeToolbar() {
+	$("#toolbar").animate({marginTop: "-44px"}, 200, function() {
+		$("#showToolbar").fadeIn().css("display", "block");
+	});
+}
+
+$("#drawerToggle").click(function(event) {
+	if(drawerIsOpen) {
+		closeDrawer();
+		drawerIsOpen = false;
+	}  else {
+		openDrawer();
+		drawerIsOpen = true;
+	}
+	event.preventDefault();
+});
+
+$("#hideToolbar").click(function(event) {
+	if (drawerIsOpen) {
+		closeDrawer(function() {
+			closeToolbar();
+		});
+	} else {
+		closeToolbar();
+	}
+	event.preventDefault();
+});
+
+$("#showToolbar").find("a").click(function(event) {
+	openToolbar();
+	event.preventDefault();
+});
+
+$('.tip').tipsy();
+$.fn.tipsy.defaults = {
+	fade: true,
+	gravity: "n",
+	opacity: 0.75
+}
+
 $(function() {
 	var storage = $.localStorage;
 	var browserSettings = {
@@ -47,7 +108,7 @@ $(function() {
 		alert("TO DO: add storeOnServer() logic.\n" + json.alertHigh + "\n" + json.alertLow);
 		// reference: http://code.tutsplus.com/tutorials/submit-a-form-without-page-refresh-using-jquery--net-59
 	}
-  $.ajax('/api/v1/status.json', { success: function (xhr) {
-    $('.version').text(xhr.version);
-  }});
+$.ajax('/api/v1/status.json', { success: function (xhr) {
+	$('.version').text(xhr.version);
+}});
 });
