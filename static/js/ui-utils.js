4d975712e122f3fc8742a8858a6956b44381286b
Brian Hanifin <code@brianhanifin.com>
2014-07-28 17:30:18 -0700

    Committing important unsaved changes to ui-utils.js.

--- static/js/ui-utils.js
+++ static/js/ui-utils.js
@@ -182,6 +182,26 @@ var querystring = getQueryParms();
 // var serverSettings = getServerSettings();
 var browserSettings = getBrowserSettings(browserStorage);
 
+function Dropdown(el) {
+	this.ddmenuitem = 0;
+
+	this.$el = $(el);
+	var that = this;
+
+	$(document).click(function() { that.close(); });
+}
+Dropdown.prototype.close = function () {
+	if (this.ddmenuitem) {
+		this.ddmenuitem.css('visibility', 'hidden');
+		this.ddmenuitem = 0;
+	}
+};
+Dropdown.prototype.open = function (e) {
+	this.close();
+	this.ddmenuitem = $(this.$el).css('visibility', 'visible');
+	e.stopPropagation();
+};
+
 
 $("#drawerToggle").click(function(event) {
 	if(drawerIsOpen) {
