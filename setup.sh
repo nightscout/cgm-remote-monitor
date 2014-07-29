2593d49c4eccd37bd2f69fe6b6e65971d92e0c34
colmlennon <colm@hakaproducts.com>
2014-07-29 16:59:15 -0500

    Revert "Update to Remote Monitor"

--- setup.sh
+++
@@ -1,7 +0,0 @@
-sudo apt-get update
-sudo apt-get install -y python-software-properties python g++ make git
-sudo add-apt-repository ppa:chris-lea/node.js
-sudo apt-get update
-sudo apt-get install nodejs
-
-npm install
