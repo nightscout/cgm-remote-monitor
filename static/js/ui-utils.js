2e69e44eec71fce3f8404ae43ab5837c034e53b2
John Costik <hackingtype1@gmail.com>
2014-08-10 20:45:10 -0400

    testing confimation

--- static/js/ui-utils.js
+++ static/js/ui-utils.js
@@ -232,33 +232,33 @@ function treatmentSubmit() {
             var sensor = document.getElementById("sensor").value;
             var meter = document.getElementById("meter").value;
 
-            var ok = confirm('Please verify that the data entered is correct: ' + '\nEntered By: ' + enteredBy + '\nEvent type: ' + eventType + '\nBlood glucose: ' + glucoseValue + '\nMethod: ' + method + '\nCarbs Given: ' + carbsGiven + '\nInsulin Given: ' + insulinGiven + '\nNotes: ' + notes);
+            var ok = window.confirm('Please verify that the data entered is correct: ' + '\nEntered By: ' + enteredBy + '\nEvent type: ' + eventType + '\nBlood glucose: ' + glucoseValue + '\nMethod: ' + method + '\nCarbs Given: ' + carbsGiven + '\nInsulin Given: ' + insulinGiven + '\nNotes: ' + notes);
 
-            if (ok) {
+            //if (ok) {
 
-                //this will wire to NS api, soon
-                var client = new WindowsAzure.MobileServiceClient(
-                       "https://someazuremobileservice",
-                       "boguskey"
-                   );
+            //    //this will wire to NS api, soon
+            //    var client = new WindowsAzure.MobileServiceClient(
+            //           "https://someazuremobileservice",
+            //           "boguskey"
+            //       );
 
-                var datatable = client.getTable('EventLog');
+            //    var datatable = client.getTable('EventLog');
 
-                datatable.insert({
-                    enteredBy: enteredBy,
-                    eventType: eventType,
-                    glucoseValue: glucoseValue,
-                    method: method,
-                    carbsGiven: carbsGiven,
-                    insulinGiven: insulinGiven,
-                    notes: notes
-                }).done(function (result) {
-                    //reload
-                    
-                }, function (err) {
-                    alert("Error: Could not send data, please resubmit.");
-                });
-            }
+            //    datatable.insert({
+            //        enteredBy: enteredBy,
+            //        eventType: eventType,
+            //        glucoseValue: glucoseValue,
+            //        method: method,
+            //        carbsGiven: carbsGiven,
+            //        insulinGiven: insulinGiven,
+            //        notes: notes
+            //    }).done(function (result) {
+            //        //reload
+            //        
+            //    }, function (err) {
+            //        alert("Error: Could not send data, please resubmit.");
+            //    });
+            //}
 
             return false;
         }
