68cbc057430742350ed9beaafb7c06c88fb254b5
andrewbs <githubabs@jump-run.com>
2014-08-20 14:49:48 +1000

    Update ui-utils.js

--- static/js/ui-utils.js
+++ static/js/ui-utils.js
@@ -358,6 +358,7 @@ $(function() {
 // Convert mmol <--> mgdl //
 $("#lowMg").change(function() {
 	var MgtoMm = $("lowMg").val();
+	alert("LowMg"+MgtoMm);
 	$("lowMm").val((MgtoMm/18.0192).toFixed(1));
 });
 $("#hiMg").change(function() {
