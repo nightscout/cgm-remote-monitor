c1184f5ccc1f1473f7a7ad9424726af5c5f71eea
Jason Calabrese <jason.calabrese@refworks-cos.com>
2014-08-31 23:39:15 -0700

    don't return a string the value 'undefined' if there is no battery info

--- bin/post-devicestatus.sh
+++ bin/post-devicestatus.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-curl -H "Content-Type: application/json" -XPOST 'http://localhost:1337/api/v1/devicestatus/' -d '{
+#curl -H "Content-Type: application/json" -XPOST 'http://localhost:1337/api/v1/devicestatus/' -d '{
+curl -H "Content-Type: application/json" -XPOST 'http://ns-dev2.cbrese.com/api/v1/devicestatus/' -d '{
   "uploaderBattery": 55
 }'
