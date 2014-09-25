ba7f2d34307e80f91b5e4c25e00c51c60ba8eb32
Rob Strickland <raven569@users.noreply.github.com>
2014-09-25 09:59:19 -0500

    Update client.js
    modify Alert levels: TargetTop 240, Target Bottom 70

--- static/js/client.js
+++ static/js/client.js
@@ -47,8 +47,8 @@
       .attr("class", "tooltip")
       .style("opacity", 0);
     //TODO: get these from the config
-    var targetTop = 180,
-        targetBottom = 80;
+    var targetTop = 240,
+        targetBottom = 70;
 
     var futureOpacity = d3.scale.linear( )
         .domain([TWENTY_FIVE_MINS_IN_MS, SIXTY_MINS_IN_MS])
