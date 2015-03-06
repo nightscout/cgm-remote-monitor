aa9233be1bad2c7a51f09ff80bfde3226baecfff
Jason Calabrese <jason@cbrese.com>
2015-03-06 00:24:35 -0800

    new pill display for BG delta and IOB

--- static/js/client.js
+++ static/js/client.js
@@ -335,21 +335,29 @@ var app = {}, browserSettings = {}, browserStorage = $.localStorage;
                     bgDeltaString = '+' + bgDelta;
                 }
 
+                bgDeltaString = '<span><em>' + bgDeltaString + '</em><label>';
+
                 if (browserSettings.units == 'mmol') {
-                    bgDeltaString = bgDeltaString + ' mmol/L'
+                    bgDeltaString = bgDeltaString + ' mmol/L';
                 } else {
-                    bgDeltaString = bgDeltaString + ' mg/dL'
+                    bgDeltaString = bgDeltaString + ' mg/dL';
                 }
             }
 
+            bgDeltaString += '</label></span>';
+
             return bgDeltaString;
+        }
+
+        function buildIOBIndicator(time) {
+            var iob = Nightscout.iob.calcTotal(treatments, profile, time);
+            return '<span><label>IOB</label><em>' + iob.display + '</em></span>';
         }
 
         var color = inRetroMode() ? 'grey' : sgvToColor(latestSGV.y);
 
         $('.container #noButton .currentBG').css({color: color});
         $('.container #noButton .currentDirection').css({color: color});
-        $('.container #noButton .currentDetails').css({color: color});
 
         // predict for retrospective data
         // by changing lookback from 1 to 2, we modify the AR algorithm to determine its initial slope from 10m
@@ -388,15 +396,14 @@ var app = {}, browserSettings = {}, browserStorage = $.localStorage;
                 var details = calcBGDelta(prevfocusPoint.y, focusPoint.y);
 
                 if (showIOB()) {
-                    var iob = Nightscout.iob.calcTotal(treatments, profile, time);
-                    details += ", IOB: " + iob.display;
+                    details += buildIOBIndicator(time);
                 }
 
                 currentBG.css('text-decoration','line-through');
                 currentDirection.html(focusPoint.y < 39 ? '✖' : focusPoint.direction);
-                currentDetails.text(details).css('text-decoration','line-through');
+                currentDetails.html(details);
             } else {
-                currentBG.text('---').css('text-decoration','');
+                currentBG.text('---');
                 currentDirection.text('-');
                 currentDetails.text('');
             }
@@ -418,13 +425,12 @@ var app = {}, browserSettings = {}, browserStorage = $.localStorage;
             var details = calcBGDelta(prevSGV.y, latestSGV.y);
 
             if (showIOB()) {
-                var iob = Nightscout.iob.calcTotal(treatments, profile, nowDate);
-                details += ", IOB: " + iob.display;
+                details += buildIOBIndicator(nowDate);
             }
 
             currentBG.css('text-decoration', '');
             currentDirection.html(latestSGV.y < 39 ? '✖' : latestSGV.direction);
-            currentDetails.text(details).css('text-decoration','');
+            currentDetails.html(details);
         }
 
         xScale.domain(brush.extent());
