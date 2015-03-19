ffc744514c32196d28ee5d012ebf4691e7462415
MilosKozak <m.kozak@sysop.cz>
2015-03-19 21:00:58 +0100

    Browser defaults

--- static/js/client.js
+++ static/js/client.js
@@ -1671,6 +1671,7 @@ var app = {}, browserSettings = {}, browserStorage = $.localStorage;
                 , alarm_types: xhr.alarm_types
                 , units: xhr.units
                 , careportalEnabled: xhr.careportalEnabled
+                , defaults: xhr.defaults
             };
         }
     }).done(function() {
