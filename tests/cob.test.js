e5a2c84b6765ad0dcd084f07f18de8b74ca6ea16
Jason Calabrese <jason@cbrese.com>
2015-06-27 12:11:28 -0700

    fix COB pill formatting; add more tests

--- tests/cob.test.js
+++ tests/cob.test.js
@@ -63,5 +63,29 @@ describe('COB', function ( ) {
     result5.cob.should.equal(0);
   });
 
+  it('set a pill to the current COB', function (done) {
+
+    var app = {};
+    var clientSettings = {};
+
+    var data = {
+      treatments: [{carbs: "8", "created_at": new Date()}]
+      , profile: require('../lib/profilefunctions')([profileData])
+    };
+
+    var pluginBase = {
+      updatePillText: function mockedUpdatePillText (plugin, options) {
+        options.value.should.equal('8g');
+        done();
+      }
+    };
+
+    var sandbox = require('../lib/sandbox')();
+    var sbx = sandbox.clientInit(app, clientSettings, Date.now(), pluginBase, data);
+    cob.setProperties(sbx);
+    cob.updateVisualisation(sbx);
+
+  });
+
 
 });
