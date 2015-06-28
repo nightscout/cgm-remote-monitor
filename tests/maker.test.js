c931d03a57a3c3accdee5e1d957e5fb8e4083945
Jason Calabrese <jason@cbrese.com>
2015-06-28 12:45:58 -0700

    added more maker event names and tests

---
+++ tests/maker.test.js
@@ -0,0 +1,35 @@
+var should = require('should');
+
+describe('maker', function ( ) {
+  var maker = require('../lib/maker')({extendedSettings: {maker: {key: '12345'}}});
+
+  //prevent any calls to iftt
+  maker.makeRequest = function noOpMakeRequest (event, callback) {};
+
+  it('turn values to a query', function (done) {
+    maker.valuesToQuery(['This is a title', 'This is the message']).should.equal('?value1=This%20is%20a%20title&value2=This%20is%20the%20message');
+    done();
+  });
+
+  it('send a request', function (done) {
+    maker.makeRequest = function mockedMakeRequest ( ) { done(); };
+    maker.sendEvent({name: 'test'}, function sendCallback (err) {
+      should.not.exist(err);
+    });
+  });
+
+  it('send a allclear, but only once', function (done) {
+    maker.makeRequest = function mockedMakeRequest ( ) { done(); };
+    maker.sendAllClear(function sendCallback (err, result) {
+      should.not.exist(err);
+      result.sent.should.equal(true);
+    });
+
+    //send again, if done is called again test will fail
+    maker.sendAllClear(function sendCallback (err, result) {
+      should.not.exist(err);
+      result.sent.should.equal(false);
+    });
+  });
+
+});
