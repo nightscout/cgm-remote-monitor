c457c8e71ace96c69fa9fc8cd9d2c38def1d48c7
Jason Calabrese <jason@cbrese.com>
2015-07-09 00:14:07 -0700

    make the current/stale bg states render correctly

--- static/js/client.js
+++ static/js/client.js
@@ -1391,8 +1391,9 @@ var app = {}, browserSettings = {}, browserStorage = $.localStorage;
       checkTimeAgoAlarm(ago);
     }
 
+    $('#container').toggleClass('alarming-timeago', ago.status !== 'current');
+
     if (alarmingNow() && ago.status === 'current' && isTimeAgoAlarmType(currentAlarmType)) {
-      $('#container').removeClass('alarming-timeago');
       stopAlarm(true, ONE_MIN_IN_MS);
     }
 
