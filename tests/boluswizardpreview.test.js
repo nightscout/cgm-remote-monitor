642e2af38372bdee15ded2b938f6817cd76cfe02
Jason Calabrese <jason@cbrese.com>
2015-07-08 20:02:13 -0700

    prevent snoozes from stacking; get DIA by time for iob cals; more tests

--- tests/boluswizardpreview.test.js
+++ tests/boluswizardpreview.test.js
@@ -1,21 +1,22 @@
 var should = require('should');
+var Stream = require('stream');
 
 describe('boluswizardpreview', function ( ) {
 
   var boluswizardpreview = require('../lib/plugins/boluswizardpreview')();
   var ar2 = require('../lib/plugins/ar2')();
+  var iob = require('../lib/plugins/iob')();
   var delta = require('../lib/plugins/delta')();
 
   var env = require('../env')();
+  env.testMode = true;
   var ctx = {};
   ctx.data = require('../lib/data')(env, ctx);
   ctx.notifications = require('../lib/notifications')(env, ctx);
 
   function prepareSandbox ( ) {
     var sbx = require('../lib/sandbox')().serverInit(env, ctx);
-    sbx.offerProperty('iob', function () {
-      return {iob: 0};
-    });
+    iob.setProperties(sbx);
     ar2.setProperties(sbx);
     delta.setProperties(sbx);
     boluswizardpreview.setProperties(sbx);
@@ -30,7 +31,8 @@ describe('boluswizardpreview', function ( ) {
   var before = now - (5 * 60 * 1000);
 
   var profile = {
-    sens: 90
+    dia: 3
+    , sens: 90
     , target_high: 120
     , target_low: 100
   };
@@ -38,6 +40,7 @@ describe('boluswizardpreview', function ( ) {
   it('Not trigger an alarm when in range', function (done) {
     ctx.notifications.initRequests();
     ctx.data.sgvs = [{x: before, y: 95}, {x: now, y: 100}];
+    ctx.data.treatments = [];
     ctx.data.profiles = [profile];
 
     var sbx = prepareSandbox();
@@ -51,6 +54,7 @@ describe('boluswizardpreview', function ( ) {
   it('trigger a warning when going out of range', function (done) {
     ctx.notifications.initRequests();
     ctx.data.sgvs = [{x: before, y: 175}, {x: now, y: 180}];
+    ctx.data.treatments = [];
     ctx.data.profiles = [profile];
 
     var sbx = prepareSandbox();
@@ -59,13 +63,14 @@ describe('boluswizardpreview', function ( ) {
     var highest = ctx.notifications.findHighestAlarm();
     highest.level.should.equal(ctx.notifications.levels.WARN);
     highest.title.should.equal('Warning, Check BG, time to bolus?');
-    highest.message.should.equal('BG Now: 180 +5 ↗ mg/dl\nBG 15m: 187 mg/dl\nBWP: 0.66U');
+    highest.message.should.equal('BG Now: 180 +5 ↗ mg/dl\nBG 15m: 187 mg/dl\nBWP: 0.66U\nIOB: 0U');
     done();
   });
 
   it('trigger an urgent alarms when going too high', function (done) {
     ctx.notifications.initRequests();
     ctx.data.sgvs = [{x: before, y: 295}, {x: now, y: 300}];
+    ctx.data.treatments = [];
     ctx.data.profiles = [profile];
 
     var sbx = prepareSandbox();
@@ -73,6 +78,32 @@ describe('boluswizardpreview', function ( ) {
     ctx.notifications.findHighestAlarm().level.should.equal(ctx.notifications.levels.URGENT);
 
     done();
+  });
+
+  it('request a snooze when there is enough IOB', function (done) {
+
+    ctx.notifications.resetStateForTests();
+    ctx.notifications.initRequests();
+    ctx.data.sgvs = [{x: before, y: 295}, {x: now, y: 300}];
+    ctx.data.treatments = [{created_at: before, insulin: '5.0'}];
+    ctx.data.profiles = [profile];
+
+    var sbx = prepareSandbox();
+
+    //start fresh to we don't pick up other notifications
+    ctx.bus = new Stream;
+    //if notification doesn't get called test will time out
+    ctx.bus.on('notification', function callback (notify) {
+      notify.clear.should.equal(true);
+      if (notify.clear) {
+        done();
+      }
+    });
+
+    ar2.checkNotifications(sbx);
+    boluswizardpreview.checkNotifications(sbx);
+    ctx.notifications.process();
+
   });
 
 });
