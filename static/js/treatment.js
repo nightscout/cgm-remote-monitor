91d2c0c77815c1fa0490b1b036465b06bd36b6ad
Jason Calabrese <jason@cbrese.com>
2015-07-15 22:59:19 -0700

    use moment.js for all date parsing+manipulation; add test; wrap in clousure; adjust size of inputs

--- static/js/treatment.js
+++ static/js/treatment.js
@@ -1,118 +1,120 @@
 'use strict';
 
-function initTreatmentDrawer()  {
-  $('#eventType').val('BG Check');
-  $('#glucoseValue').val('').attr('placeholder', 'Value in ' + browserSettings.units);
-  $('#meter').prop('checked', true);
-  $('#carbsGiven').val('');
-  $('#insulinGiven').val('');
-  $('#preBolus').val(0);
-  $('#notes').val('');
-  $('#enteredBy').val(browserStorage.get('enteredBy') || '');
-  $('#nowtime').prop('checked', true);
-  $('#eventTimeValue').val(new Date().toTimeString().slice(0,5));
-  $('#eventDateValue').val(Nightscout.utils.toDateInputValue(new Date()));
-}
-
-function treatmentSubmit(event) {
-
-  var data = {};
-  data.enteredBy = $('#enteredBy').val();
-  data.eventType = $('#eventType').val();
-  data.glucose = $('#glucoseValue').val();
-  data.glucoseType = $('#treatment-form input[name=glucoseType]:checked').val();
-  data.carbs = $('#carbsGiven').val();
-  data.insulin = $('#insulinGiven').val();
-  data.preBolus = parseInt($('#preBolus').val());
-  data.notes = $('#notes').val();
-  data.units = browserSettings.units;
-
-  var errors = [];
-  if (isNaN(data.glucose)) {
-    errors.push('Blood glucose must be a number');
+(function () {
+  function initTreatmentDrawer() {
+    $('#eventType').val('BG Check');
+    $('#glucoseValue').val('').attr('placeholder', 'Value in ' + browserSettings.units);
+    $('#meter').prop('checked', true);
+    $('#carbsGiven').val('');
+    $('#insulinGiven').val('');
+    $('#preBolus').val(0);
+    $('#notes').val('');
+    $('#enteredBy').val(browserStorage.get('enteredBy') || '');
+    $('#nowtime').prop('checked', true);
+    $('#eventTimeValue').val(moment().format('HH:mm'));
+    $('#eventDateValue').val(moment().format('YYYY-MM-D'));
   }
 
-  if (isNaN(data.carbs)) {
-    errors.push('Carbs must be a number');
+  function treatmentSubmit(event) {
+
+    var data = {};
+    data.enteredBy = $('#enteredBy').val();
+    data.eventType = $('#eventType').val();
+    data.glucose = $('#glucoseValue').val();
+    data.glucoseType = $('#treatment-form input[name=glucoseType]:checked').val();
+    data.carbs = $('#carbsGiven').val();
+    data.insulin = $('#insulinGiven').val();
+    data.preBolus = parseInt($('#preBolus').val());
+    data.notes = $('#notes').val();
+    data.units = browserSettings.units;
+
+    var errors = [];
+    if (isNaN(data.glucose)) {
+      errors.push('Blood glucose must be a number');
+    }
+
+    if (isNaN(data.carbs)) {
+      errors.push('Carbs must be a number');
+    }
+
+    if (isNaN(data.insulin)) {
+      errors.push('Insulin must be a number');
+    }
+
+    if (errors.length > 0) {
+      window.alert(errors.join('\n'));
+    } else {
+      if ($('#othertime').is(':checked')) {
+        data.eventTime = Nightscout.utils.mergeInputTime($('#eventTimeValue').val(), $('#eventDateValue').val());
+      }
+      confirmPost(data);
+    }
+
+    if (event) {
+      event.preventDefault();
+    }
   }
 
-  if (isNaN(data.insulin)) {
-    errors.push('Insulin must be a number');
+  function confirmPost(data) {
+    var confirmtext =
+      'Please verify that the data entered is correct: ' +
+      '\nEvent type: ' + data.eventType;
+    confirmtext += data.glucose ? '\nBlood glucose: ' + data.glucose + '\nMethod: ' + data.glucoseType : '';
+    confirmtext += data.carbs ? '\nCarbs Given: ' + data.carbs : '';
+    confirmtext += data.insulin ? '\nInsulin Given: ' + data.insulin : '';
+    confirmtext += data.preBolus ? '\nPre Bolus: ' + data.preBolus : '';
+    confirmtext += data.notes ? '\nNotes: ' + data.notes : '';
+    confirmtext += data.enteredBy ? '\nEntered By: ' + data.enteredBy : '';
+    confirmtext += data.eventTime ? '\nEvent Time: ' + data.eventTime.format('LLL') : '';
+
+    if (window.confirm(confirmtext)) {
+      var dataJson = JSON.stringify(data, null, ' ');
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST', '/api/v1/treatments/', true);
+      xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+      xhr.send(dataJson);
+
+      browserStorage.set('enteredBy', data.enteredBy);
+
+      closeDrawer('#treatmentDrawer');
+    }
   }
 
-  if (errors.length > 0) {
-    window.alert(errors.join('\n'));
-  } else {
-		if ($('#othertime').is(':checked')) {
-			data.eventTime = Nightscout.utils.mergeInputTime($('#eventTimeValue').val(),$('#eventDateValue').val());
-		}
-    confirmPost(data);
-  }
-
-  if (event) {
+  $('#treatmentDrawerToggle').click(function (event) {
+    toggleDrawer('#treatmentDrawer', initTreatmentDrawer);
     event.preventDefault();
-  }
-}
+  });
 
-function confirmPost(data) {
-  var confirmtext = 
-        'Please verify that the data entered is correct: ' +
-        '\nEvent type: ' + data.eventType;
-  confirmtext += data.glucose ? '\nBlood glucose: ' + data.glucose + '\nMethod: ' + data.glucoseType : '';
-  confirmtext += data.carbs ? '\nCarbs Given: ' + data.carbs : '';
-  confirmtext += data.insulin ? '\nInsulin Given: ' + data.insulin : '';
-  confirmtext += data.preBolus ? '\nPre Bolus: ' + data.preBolus : '';
-  confirmtext += data.notes ? '\nNotes: ' + data.notes : '';
-  confirmtext += data.enteredBy ? '\nEntered By: ' + data.enteredBy : '';
-  confirmtext += data.eventTime ? '\nEvent Time: ' + data.eventTime.toLocaleString(): '';
+  $('#treatmentDrawer').find('button').click(treatmentSubmit);
 
-  if (window.confirm(confirmtext)) {
-    var dataJson = JSON.stringify(data, null, ' ');
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', '/api/v1/treatments/', true);
-    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-    xhr.send(dataJson);
+  $('#eventTime input:radio').change(function (event) {
+    if ($('#othertime').is(':checked')) {
+      $('#eventTimeValue').focus();
+    }
+    event.preventDefault();
+  });
 
-    browserStorage.set('enteredBy', data.enteredBy);
+  $('.eventinput').focus(function (event) {
+    $('#othertime').prop('checked', true);
+    var moment = Nightscout.utils.mergeInputTime($('#eventTimeValue').val(), $('#eventDateValue').val());
+    $(this).attr('oldminutes', moment.minutes());
+    $(this).attr('oldhours', moment.hours());
+    event.preventDefault();
+  });
 
-    closeDrawer('#treatmentDrawer');
-  }
-}
-
-$('#treatmentDrawerToggle').click(function(event) {
-  toggleDrawer('#treatmentDrawer', initTreatmentDrawer);
-  event.preventDefault();
-});
-
-$('#treatmentDrawer').find('button').click(treatmentSubmit);
-
-$('#eventTime input:radio').change(function (event){
-  if ($('#othertime').is(':checked')) {
-    $('#eventTimeValue').focus();
-  }
-  event.preventDefault();
-});
-
-$('.eventtimeinput').focus(function (event) {
-  $('#othertime').prop('checked', true);
-  var time = Nightscout.utils.mergeInputTime($('#eventTimeValue').val(),$('#eventDateValue').val());
-  $(this).attr('oldminutes',time.getMinutes());
-  $(this).attr('oldhours',time.getHours());
-  event.preventDefault();
-});
-
-$('.eventtimeinput').change(function (event) {
-  $('#othertime').prop('checked', true);
-  var time = Nightscout.utils.mergeInputTime($('#eventTimeValue').val(),$('#eventDateValue').val());
-  if ($(this).attr('oldminutes')==='59' && time.getMinutes()===0) {
-     Nightscout.utils.addHours(time,1);
-  }
-  if ($(this).attr('oldminutes')==='0' && time.getMinutes()===59) {
-     Nightscout.utils.addHours(time,-1);
-  }
-  $('#eventTimeValue').val(time.toTimeString().slice(0,5));
-  $('#eventDateValue').val(Nightscout.utils.toDateInputValue(time));
-  $(this).attr('oldminutes',time.getMinutes());
-  $(this).attr('oldhours',time.getHours());
-  event.preventDefault();
-});
+  $('.eventinput').change(function (event) {
+    $('#othertime').prop('checked', true);
+    var moment = Nightscout.utils.mergeInputTime($('#eventTimeValue').val(), $('#eventDateValue').val());
+    if ($(this).attr('oldminutes') === ' 59' && moment.minutes() === 0) {
+      moment.add(1, 'hours');
+    }
+    if ($(this).attr('oldminutes') === '0' && moment.minutes() === 59) {
+      moment.add(-1, 'hours');
+    }
+    $('#eventTimeValue').val(moment.format('HH:mm'));
+    $('#eventDateValue').val(moment.format('YYYY-MM-D'));
+    $(this).attr('oldminutes', moment.minutes());
+    $(this).attr('oldhours', moment.hours());
+    event.preventDefault();
+  });
+})();
