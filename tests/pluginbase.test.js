413e7c98618405e841693c45e699d3dff38951aa
Jason Calabrese <jason@cbrese.com>
2015-07-18 23:28:28 -0700

    use jsdom to be able to create a test for pluginbase using jquery

---
+++ tests/pluginbase.test.js
@@ -0,0 +1,47 @@
+'use strict';
+
+require('should');
+
+describe('pluginbase', function ( ) {
+
+  //jsdom();
+
+  var jsdom = require("jsdom").jsdom;
+  var doc = jsdom("hello world");
+  var window = doc.parentWindow;
+
+
+  global.$ = global.jQuery = require('jquery')(window);
+
+  function div (clazz) {
+    return $('<div class="' + clazz + '"></div>');
+  }
+
+  var container = div('container')
+    , bgStatus = div('bgStatus').appendTo(container)
+    , currentBG = div('currentBG').appendTo(bgStatus)
+    , majorPills = div('majorPills').appendTo(bgStatus)
+    , minorPills = div('minorPills').appendTo(bgStatus)
+    , statusPills = div('statusPills').appendTo(bgStatus)
+    , tooltip = div('tooltip').appendTo(container)
+    ;
+
+  var fake = {
+    name: 'fake'
+    , label: 'Insulin-on-Board'
+    , pluginType: 'pill-major'
+  };
+
+  it('does stuff', function() {
+    var pluginbase = require('../lib/plugins/pluginbase')(majorPills, minorPills, statusPills, bgStatus, tooltip);
+
+    pluginbase.updatePillText(fake, {
+      value: '123'
+      , label: 'TEST'
+      , info: [{label: 'Label', value: 'Value'}]
+    });
+
+    majorPills.length.should.equal(1);
+  });
+
+});
