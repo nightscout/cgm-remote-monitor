da9cceb548ec326d89cd286ad9886833ce7e11f0
MilosKozak <m.kozak@sysop.cz>
2015-09-16 22:28:36 +0200

    admin tools initial release

---
+++ static/admin/js/admin.js
@@ -0,0 +1,21 @@
+(function () {
+  'use strict';
+  //for the tests window isn't the global object
+  var $ = window.$;
+  var _ = window._;
+  var moment = window.moment;
+  var Nightscout = window.Nightscout;
+  var client = Nightscout.client;
+  var translate = client.translate;
+  var admin_plugins = Nightscout.admin_plugins;
+
+  if (serverSettings === undefined) {
+    console.error('server settings were not loaded, will not call init');
+  } else {
+    client.init(serverSettings, Nightscout.plugins);
+  }
+  
+  // init HTML code
+  admin_plugins.createHTML( client );
+
+})();
