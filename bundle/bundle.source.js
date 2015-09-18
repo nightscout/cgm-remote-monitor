499fa499bb01d3bbaf39fbdfc6bb19069385c0c9
moose831 <rodney.fleig@gmail.com>
2015-09-18 09:35:16 -0700

    Revert "funnel cake 0.8.1"

--- bundle/bundle.source.js
+++ bundle/bundle.source.js
@@ -7,9 +7,7 @@
 
   window.Nightscout = {
     client: require('../lib/client')
-    , units: require('../lib/units')()
     , plugins: require('../lib/plugins/')().registerClientDefaults()
-    , report_plugins: require('../lib/report_plugins/')()
   };
 
   console.info('Nightscout bundle ready');
