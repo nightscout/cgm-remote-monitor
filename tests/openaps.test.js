a7bc56e329b3783958b7e8f9cba49910a9af8648
Jason Calabrese <jason@cbrese.com>
2015-12-07 19:52:06 -0800

    display last loop time in the pill instead of last update time
    also check for an offline marker when determining the status level/class

--- tests/openaps.test.js
+++ tests/openaps.test.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var _ = require('lodash');
 var should = require('should');
 var moment = require('moment');
 
@@ -69,7 +70,8 @@ describe('openaps', function ( ) {
       , pluginBase: {
         updatePillText: function mockedUpdatePillText(plugin, options) {
           options.label.should.equal('OpenAPS ⌁');
-          options.value.should.equal('1m ago');
+          options.value.should.equal('2m ago');
+          _.last(options.info).value.should.equal('1m ago');
           done();
         }
       }
