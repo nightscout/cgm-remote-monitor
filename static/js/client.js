15a3d8db10b8c7d1251590ac6f405693372fc7eb
Jason Calabrese <jason@cbrese.com>
2016-08-01 23:15:53 -0700

    only set authorized.lat if authorized

--- static/js/client.js
+++ static/js/client.js
@@ -3,5 +3,8 @@
 if (serverSettings === undefined) {
   console.error('server settings were not loaded, will not call init');
 } else {
+  if (authorized) {
+    authorized.lat = Date.now();
+  }
   window.Nightscout.client.init(serverSettings, Nightscout.plugins, authorized);
 }
