0f42e692699edc133703af863ebfbf6e96521669
Ben West <bewest@gmail.com>
2016-08-09 12:43:43 -0700

    env: off/on are truthy sometimes
    Provide readENVTruthy, and mapTruthy, to explicitly handle
    the cases where on/off or similar should result in a
    Boolean value.  Here, they should be mapped correctly,
    eliminating a problem @miloskozak experienced when setting
    AUTH_DEFAULT_ROLES=bad, there is an exception becaue the
    value is a literal `true` instead of the string `"bad"`.
    
    Intended to be a no-op, preserving the intended effects
    from previous commit
    dbe29bd4bee725649073c8486b52eac739846305 from
    @jasoncalabrese.

--- env.js
+++ env.js
@@ -136,8 +136,8 @@ function updateSettings() {
   //should always find extended settings last
   env.extendedSettings = findExtendedSettings(process.env);
 
-  if (!readENV('TREATMENTS_AUTH', true)) {
-    env.settings.authDefaultRoles = env.settings.authDefaultRoles || [ ];
+  if (!readENVTruthy('TREATMENTS_AUTH', true)) {
+    env.settings.authDefaultRoles = env.settings.authDefaultRoles || "";
     env.settings.authDefaultRoles += ' careportal';
   }
 
@@ -151,10 +151,15 @@ function readENV(varName, defaultValue) {
     || process.env[varName]
     || process.env[varName.toLowerCase()];
 
-  if (typeof value === 'string' && (value.toLowerCase() === 'on' || value.toLowerCase() === 'true')) { value = true; }
-  if (typeof value === 'string' && (value.toLowerCase() === 'off' || value.toLowerCase() === 'false')) { value = false; }
 
   return value != null ? value : defaultValue;
+}
+
+function readENVTruthy(varName, defaultValue) {
+  var value = readENV(varName, defaultValue);
+  if (typeof value === 'string' && (value.toLowerCase() === 'on' || value.toLowerCase() === 'true')) { value = true; }
+  if (typeof value === 'string' && (value.toLowerCase() === 'off' || value.toLowerCase() === 'false')) { value = false; }
+  return value;
 }
 
 function findExtendedSettings (envs) {
