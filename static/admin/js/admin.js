46bc6eda6b1ffcdd6defa228beb5e711faaaae21
Jason Calabrese <jason@cbrese.com>
2016-10-09 00:13:15 -0700

    add option for add a requiredPermission to the client, use that for admin page to support unstored api-secrets

--- static/admin/js/admin.js
+++ static/admin/js/admin.js
@@ -5,6 +5,7 @@
   var client = Nightscout.client;
   var admin_plugins = Nightscout.admin_plugins;
 
+  client.requiredPermission = '*';
   client.init(Nightscout.plugins, function loaded () {
     // init HTML code
     admin_plugins.createHTML( client );
