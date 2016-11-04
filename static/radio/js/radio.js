b7f1257c3e9e0dcce3db103beeaefcdf32745feb
Jason Calabrese <jason@cbrese.com>
2016-11-04 01:31:18 -0700

    codacy clean up

--- static/radio/js/radio.js
+++ static/radio/js/radio.js
@@ -39,21 +39,23 @@ window.Nightscout.client.init(function loaded() {
   function createLoop (synth, sgvs) {
     function callback (time, note) {
       console.log(time, note);
-      synth.triggerAttackRelease(note, "16n",  time);
+      synth.triggerAttackRelease(note, '16n',  time);
     }
-    var seq = new Tone.Sequence(callback, sgvs, "16n");
+    var seq = new Tone.Sequence(callback, sgvs, '16n');
     seq.loop = false;
     return seq;
   }
 
   function glucose (sgv) {
-    if (sgv)
-    return parseInt(sgv.mgdl || sgv.sgv || sgv.glucose || 35)
-    return 20
+    if (sgv) {
+      return parseInt(sgv.mgdl || sgv.sgv || sgv.glucose || 35);
+    }
+
+    return 20;
   }
 
   $(document).ready(function ( ) {
-    console.log("OK");
+    console.log('OK');
     var converter = convert( );
     var synth = new Tone.PolySynth(16, Tone.MonoSynth);
     // default volume always makes my ears bleed
@@ -62,19 +64,19 @@ window.Nightscout.client.init(function loaded() {
     Tone.Transport.timeSignature = [ 3, 2 ];
     Tone.Transport.bpm.value = 320;
 
-    function play_next (time) {
-      var sgv = sgvs.shift( );
-      console.log(sgv);
-      if (!sgv) {
-        loop.stop( );
-      }
-      if (sgv) {
-        var freq = converter.freq(sgv.mgdl || 30);
-        synth.triggerAttackRelease(parseInt(sgv.mgdl || sgv.sgv || sgv.glucose || 39) * 4, "8n", time);
-      }
-    }
+    // function play_next (time) {
+    //   var sgv = sgvs.shift( );
+    //   console.log(sgv);
+    //   if (!sgv) {
+    //     loop.stop( );
+    //   }
+    //   if (sgv) {
+    //     var freq = converter.freq(sgv.mgdl || 30);
+    //     synth.triggerAttackRelease(parseInt(sgv.mgdl || sgv.sgv || sgv.glucose || 39) * 4, '8n', time);
+    //   }
+    // }
 
-    // var loop = new Tone.Loop(play_next, "4n");
+    // var loop = new Tone.Loop(play_next, '4n');
     var loop;
     function play_data ( ) {
       var sgvs = Nightscout.client.sbx.data.sgvs.slice( ).map(glucose).map(converter.freq);
