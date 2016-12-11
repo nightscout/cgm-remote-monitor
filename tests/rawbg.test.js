b4858b6d0b1d607055037c8fffdcb46277b96710
Rick Friele <friele@gmail.com>
2016-12-11 10:54:41 -0800

    Bug fixes and add unit tests

--- tests/rawbg.test.js
+++ tests/rawbg.test.js
@@ -54,4 +54,82 @@ describe('Raw BG', function ( ) {
 
   });
 
+  it('should handle Google Home requests', function(done) {
+
+    var sandbox = require('../lib/sandbox')();
+    var sbx = sandbox.clientInit(ctx, Date.now(), data);
+
+    rawbg.setProperties(sbx);
+
+    rawbg.googleHome.intentHandlers.length.should.equal(2);
+
+    var req = {
+      "id": "b6b30104-0629-408a-b781-9fc265ac1df1",
+      "timestamp": "2016-12-11T18:44:51.422Z",
+      "result": {
+        "source": "agent",
+        "resolvedQuery": "What\'s my raw blood glucose?",
+        "action": "",
+        "actionIncomplete": false,
+        "parameters": {
+          "readingType": "raw blood glucose"
+        },
+        "contexts": [],
+        "metadata": {
+          "intentId": "c57a04a2-2f0c-4f86-b1cc-8af4c3314d6c",
+          "webhookUsed": "false",
+          "webhookForSlotFillingUsed": "false",
+          "intentName": "current_raw_bg"
+        },
+        "score": 1
+      },
+      "status": {
+        "code": 200,
+        "errorType": "success"
+      },
+      "sessionId": "4a7727af-90ce-4a1a-9732-0cc67c1dedf3"
+    }
+
+    rawbg.googleHome.intentHandlers[0].intentName.should.equal('current_raw_bg');
+    rawbg.googleHome.intentHandlers[0].intentHandler(req.result, function next(response) {
+      response.should.equal('Your current raw blood glucose is 113');
+    }, sbx);
+
+    var reqGivenName = {
+       "id": "28987593-8afe-4dbc-89b6-863e3c2f2186",
+       "timestamp": "2016-12-11T17:30:46.296Z",
+       "result": {
+         "source": "agent",
+         "resolvedQuery": "What\'s Rick\'s raw blood glucose?",
+         "action": "",
+         "actionIncomplete": false,
+         "parameters": {
+           "givenName": "Rick",
+           "readingType": "raw blood glucose"
+         },
+         "contexts": [],
+         "metadata": {
+           "intentId": "87a05d96-586a-4538-91bc-589d3e467e28",
+           "webhookUsed": "false",
+           "webhookForSlotFillingUsed": "false",
+           "intentName": "current_bg_given_name"
+         },
+         "score": 1
+       },
+       "status": {
+         "code": 200,
+         "errorType": "success"
+       },
+       "sessionId": "4a7727af-90ce-4a1a-9732-0cc67c1dedf3"
+     };
+
+    rawbg.googleHome.intentHandlers[1].intentName.should.equal('current_raw_bg_given_name');
+    rawbg.googleHome.intentHandlers[1].intentHandler(reqGivenName.result, function next(response) {
+      response.should.equal('Rick\'s current raw blood glucose is 113');
+
+      done();
+    }, sbx);
+
+  });
+
 });
