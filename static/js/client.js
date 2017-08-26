eda7c7743cf3b4efe6a73db1bfad2896a6e7c66a
Sulka Haro <sulka@sulka.net>
2017-08-26 11:54:25 +0300

    Trigger app on DOM ready event (as recommended by HTML5 spec). Detect if app is completely offline, auto-reload if needed.

--- static/js/client.js
+++ static/js/client.js
@@ -1,3 +1,11 @@
 'use strict';
 
-window.Nightscout.client.init();
+$(document).on('online', function() {
+	console.log('Application got online event, reloading');
+	window.location.reload();
+});
+
+$(document).ready(function() {
+	console.log('Application got ready event');
+	window.Nightscout.client.init();
+});
