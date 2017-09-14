12fe6aa45b7a192ec0846a52e4a72204393713f5
Allison <allisong4@users.noreply.github.com>
2017-09-13 20:53:27 -0400

    Update processform.js

--- static/js/processform.js
+++ static/js/processform.js
@@ -120,8 +120,10 @@ function setButtonActions(){
 				document.getElementById("submission_meal").innerHTML += "New total: "+newTotal.toFixed(2)+"<br/>";
 			}
 			else{
+				percentExt = (finalextdose/newTotal).toFixed(0);
+				percentNow = 100-percentExt;
 				var minutesToHours = (finalextbolustime/60.0).toFixed(1);
-			  	document.getElementById("submission_meal").innerHTML += "New total: "+newTotal.toFixed(2)+" ("+finalbolusnowpercent.toFixed(0)+"% / "+finalbolusextpercent.toFixed(0)+"%)<br/>"+(finalcarbdose+finalcorrdose+finalsuperdose).toFixed(2)+" + "+finalextdose.toFixed(2)+" extended over "+minutesToHours+" hours.<br/>";
+			  	document.getElementById("submission_meal").innerHTML += "New total: "+newTotal.toFixed(2)+" ("+percentNow.toFixed(0)+"% / "+percentExt.toFixed(0)+"%)<br/>"+(finalcarbdose+finalcorrdose+finalsuperdose).toFixed(2)+" + "+finalextdose.toFixed(2)+" extended over "+minutesToHours+" hours.<br/>";
 			}
 	  	} 
 	      
