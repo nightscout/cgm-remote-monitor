f93c2e58931f769905efe003cfe94700fe0d269d
BergerJohnsen <olemj@online.no>
2017-10-09 19:03:39 +0200

    Revert "Pull Request"

--- tests/basalprofileplugin.test.js
+++ tests/basalprofileplugin.test.js
@@ -8,7 +8,6 @@ describe('basalprofile', function ( ) {
     settings: {}
     , language: require('../lib/language')()
   };
-  ctx.language.set('en');
   ctx.ddata = require('../lib/data/ddata')();
   ctx.notifications = require('../lib/notifications')(env, ctx);
 
@@ -67,7 +66,7 @@ describe('basalprofile', function ( ) {
       , language: require('../lib/language')()
     };
 
-    var time = new Date('2015-06-21T00:00:00+00:00').getTime();
+    var time = new Date('2015-06-21T00:00:00').getTime();
 
 
     var sbx = sandbox.clientInit(ctx, time, data);
@@ -86,7 +85,7 @@ describe('basalprofile', function ( ) {
       , language: require('../lib/language')()
     };
 
-    var time = new Date('2015-06-21T00:00:00+00:00').getTime();
+    var time = new Date('2015-06-21T00:00:00').getTime();
 
 
     var sbx = sandbox.clientInit(ctx, time, data);
