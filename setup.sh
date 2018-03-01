e967f609ead09fb25a21fed100046bede7a84039
jcorbett80 <corbj0ga@gmail.com>
2018-03-01 14:22:04 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+rm -rf /usr/local/bin/npm /usr/local/share/man/man1/node* /usr/local/lib/dtrace/node.d ~/.npm ~/.node-gyp /opt/local/bin/node /opt/local/include/node /opt/local/lib/node_modules
+rm -rf /usr/local/lib/node*
+rm -rf /usr/local/include/node*
+rm -rf /usr/local/bin/node*
+apt-get autoremove
 
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get update
