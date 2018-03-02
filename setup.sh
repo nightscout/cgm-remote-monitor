8a4c4cd093909685646b79483a5098dc46ba1209
jcorbett80 <corbj0ga@gmail.com>
2018-03-01 18:44:49 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,21 +1,20 @@
 #!/bin/sh
-sudo su
-rm -rf /usr/local/bin/npm /usr/local/share/man/man1/node* /usr/local/lib/dtrace/node.d ~/.npm ~/.node-gyp /opt/local/bin/node /opt/local/include/node /opt/local/lib/node_modules
-rm -rf /usr/local/lib/node*
-rm -rf /usr/local/include/node*
-rm -rf /usr/local/bin/node*
-apt-get install nodejs
-apt-get autoremove
+sudo rm -rf /usr/local/bin/npm /usr/local/share/man/man1/node* /usr/local/lib/dtrace/node.d ~/.npm ~/.node-gyp /opt/local/bin/node /opt/local/include/node /opt/local/lib/node_modules
+sudo rm -rf /usr/local/lib/node*
+sudo rm -rf /usr/local/include/node*
+sudo rm -rf /usr/local/bin/node*
+sudo apt-get autoremove
 
-su pi
 
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get update
-sudo apt-get install -y nodejs
 sudo apt-get install -y python-software-properties python git
 sudo apt-get install -y build-essential
+apt-get autoremove
 
 sudo su
+apt-get install -y nodejs
 npm install
+apt-get autoremove
 
 su pi
