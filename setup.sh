73d1ca6affb5de4599c59412632a1986eb3c3fa5
jcorbett80 <corbj0ga@gmail.com>
2018-03-01 18:35:13 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
+sudo su
 rm -rf /usr/local/bin/npm /usr/local/share/man/man1/node* /usr/local/lib/dtrace/node.d ~/.npm ~/.node-gyp /opt/local/bin/node /opt/local/include/node /opt/local/lib/node_modules
 rm -rf /usr/local/lib/node*
 rm -rf /usr/local/include/node*
 rm -rf /usr/local/bin/node*
+apt-get install nodejs
 apt-get autoremove
+
+su pi
 
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get update
@@ -11,4 +15,7 @@ sudo apt-get install -y nodejs
 sudo apt-get install -y python-software-properties python git
 sudo apt-get install -y build-essential
 
+sudo su
 npm install
+
+su pi
