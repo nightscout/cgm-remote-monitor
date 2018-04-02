39f02d6a8dff13c954f8a68844918d2a855437be
jmphillips33 <jmphillips33@gmail.com>
2018-04-02 11:44:58 -0700

    Revert "Habanero 4-18"

--- tests/api.status.test.js
+++ tests/api.status.test.js
@@ -1,25 +1,27 @@
-'use strict';
 
 var request = require('supertest');
-var language = require('../lib/language')();
-
-require('should');
+var should = require('should');
 
 describe('Status REST api', function ( ) {
   var api = require('../lib/api/');
   before(function (done) {
     var env = require('../env')( );
-    env.settings.enable = ['careportal', 'rawbg'];
-    env.settings.authDefaultRoles = 'readable';
+    env.enable = "careportal rawbg";
     env.api_secret = 'this is my long pass phrase';
     this.wares = require('../lib/middleware/')(env);
+    var store = require('../lib/storage')(env);
     this.app = require('express')( );
     this.app.enable('api');
     var self = this;
-    require('../lib/bootevent')(env, language).boot(function booted (ctx) {
-      self.app.use('/api', api(env, ctx));
+    store(function ( ) {
+      var entriesStorage = require('../lib/entries').storage(env.mongo_collection, store);
+      self.app.use('/api', api(env, entriesStorage));
       done();
     });
+  });
+
+  it('should be a module', function ( ) {
+    api.should.be.ok;
   });
 
   it('/status.json', function (done) {
@@ -29,9 +31,7 @@ describe('Status REST api', function ( ) {
       .end(function (err, res)  {
         res.body.apiEnabled.should.equal(true);
         res.body.careportalEnabled.should.equal(true);
-        res.body.settings.enable.length.should.equal(2);
-        res.body.settings.enable.should.containEql('careportal');
-        res.body.settings.enable.should.containEql('rawbg');
+        res.body.enabledOptions.should.equal('careportal rawbg');
         done( );
       });
   });
@@ -42,30 +42,9 @@ describe('Status REST api', function ( ) {
       .end(function(err, res) {
         res.type.should.equal('text/html');
         res.statusCode.should.equal(200);
-        done();
+        done()
       });
   });
-
-  it('/status.svg', function (done) {
-    request(this.app)
-      .get('/api/status.svg')
-      .end(function(err, res) {
-        res.statusCode.should.equal(302);
-        done();
-      });
-  });
-
-  it('/status.txt', function (done) {
-    request(this.app)
-      .get('/api/status.txt')
-      .expect(200, 'STATUS OK')
-      .end(function(err, res) {
-        res.type.should.equal('text/plain');
-        res.statusCode.should.equal(200);
-        done();
-      });
-  });
-
 
   it('/status.js', function (done) {
     request(this.app)
@@ -74,7 +53,7 @@ describe('Status REST api', function ( ) {
         res.type.should.equal('application/javascript');
         res.statusCode.should.equal(200);
         res.text.should.startWith('this.serverSettings =');
-        done();
+        done()
       });
   });
 
@@ -84,7 +63,7 @@ describe('Status REST api', function ( ) {
       .end(function(err, res) {
         res.headers.location.should.equal('http://img.shields.io/badge/Nightscout-OK-green.png');
         res.statusCode.should.equal(302);
-        done();
+        done()
       });
   });
 
