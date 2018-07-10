dd1a3ea2949ebcb57237cab658f13e223edc9650
PieterGit <pieterg@gmail.com>
2018-07-10 23:31:33 +0200

    remove python and python-software-properties from setup.sh
    As far as I see in the current codebase there are no references to python. Should be tested with reinstall on clean pi3 install.

--- setup.sh
+++ setup.sh
@@ -2,8 +2,6 @@
 
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get update
-sudo apt-get install -y nodejs
-sudo apt-get install -y python-software-properties python git
-sudo apt-get install -y build-essential
+sudo apt-get install -y nodejs build-essential git
 
 npm install
