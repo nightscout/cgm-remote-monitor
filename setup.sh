3f13bb0070bd915da1aaa48293b5fee5d4bcaae0
Nshumansky <37027475+Nshumansky@users.noreply.github.com>
2018-07-29 15:12:37 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -sL https://deb.nodesource.com/setup_8.9.0 | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get update
 sudo apt-get install -y nodejs
 sudo apt-get install -y python-software-properties python git
