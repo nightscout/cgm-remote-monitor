9bf65d81b86f28af0bf7becf1263945b9845a099
PieterGit <pieterg@gmail.com>
2018-10-13 11:40:07 +0200

    reimplement changes from https://github.com/nightscout/cgm-remote-monitor/pull/3819 with indent=2
    https://raw.githubusercontent.com/nielsvanvelzen/nightscout/9afd22e208edc7e966c85f1dd0dab4d716b9be2d/webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,60 @@
 const path = require('path');
 const webpack = require('webpack');
 const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
+const pluginArray = [];
+const sourceMapType = 'source-map';
 
-var pluginArray = [];
+/*if (process.env.NODE_ENV !== 'development') {
+ console.log('Development environment detected, enabling Bundle Analyzer');
+ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-var sourceMapType = 'source-map';
+ pluginArray.push(new BundleAnalyzerPlugin({
+ // Can be `server`, `static` or `disabled`.
+ // In `server` mode analyzer will start HTTP server to show bundle report.
+ // In `static` mode single HTML file with bundle report will be generated.
+ // In `disabled` mode you can use this plugin to just generate Webpack Stats JSON file by setting
+ // `generateStatsFile` to `true`.
+ analyzerMode: 'static',
+ // Host that will be used in `server` mode to start HTTP server.
+ analyzerHost: '127.0.0.1',
+ // Port that will be used in `server` mode to start HTTP server.
+ analyzerPort: 8888,
+ // Path to bundle report file that will be generated in `static` mode.
+ // Relative to bundles output directory.
+ reportFilename: 'bundle_report.html',
+ // Module sizes to show in report by default.
+ // Should be one of `stat`, `parsed` or `gzip`.
+ // See "Definitions" section for more information.
+ defaultSizes: 'parsed',
+ // Automatically open report in default browser
+ openAnalyzer: true,
+ // If `true`, Webpack Stats JSON file will be generated in bundles output directory
+ generateStatsFile: false,
+ // Name of Webpack Stats JSON file that will be generated if `generateStatsFile` is `true`.
+ // Relative to bundles output directory.
+ statsFilename: 'stats.json',
+ // Options for `stats.toJson()` method.
+ // For example you can exclude sources of your modules from stats file with `source: false` option.
+ // See more options here: https://github.com/webpack/webpack/blob/webpack-1/lib/Stats.js#L21
+ statsOptions: null,
+ // Log level. Can be 'info', 'warn', 'error' or 'silent'.
+ logLevel: 'info'
+ }));
+ }*/
 
-if (process.env.NODE_ENV !== 'development') {
-
-  /*
-   console.log('Development environment detected, enabling Bundle Analyzer');
-
-   var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
-   pluginArray.push(new BundleAnalyzerPlugin({
-   // Can be `server`, `static` or `disabled`.
-   // In `server` mode analyzer will start HTTP server to show bundle report.
-   // In `static` mode single HTML file with bundle report will be generated.
-   // In `disabled` mode you can use this plugin to just generate Webpack Stats JSON file by setting
-   // `generateStatsFile` to `true`.
-   analyzerMode: 'static',
-   // Host that will be used in `server` mode to start HTTP server.
-   analyzerHost: '127.0.0.1',
-   // Port that will be used in `server` mode to start HTTP server.
-   analyzerPort: 8888,
-   // Path to bundle report file that will be generated in `static` mode.
-   // Relative to bundles output directory.
-   reportFilename: 'bundle_report.html',
-   // Module sizes to show in report by default.
-   // Should be one of `stat`, `parsed` or `gzip`.
-   // See "Definitions" section for more information.
-   defaultSizes: 'parsed',
-   // Automatically open report in default browser
-   openAnalyzer: true,
-   // If `true`, Webpack Stats JSON file will be generated in bundles output directory
-   generateStatsFile: false,
-   // Name of Webpack Stats JSON file that will be generated if `generateStatsFile` is `true`.
-   // Relative to bundles output directory.
-   statsFilename: 'stats.json',
-   // Options for `stats.toJson()` method.
-   // For example you can exclude sources of your modules from stats file with `source: false` option.
-   // See more options here: https://github.com/webpack/webpack/blob/webpack-1/lib/Stats.js#L21
-   statsOptions: null,
-   // Log level. Can be 'info', 'warn', 'error' or 'silent'.
-   logLevel: 'info'
-   }));
-   */
-}
-
-var jq = new webpack.ProvidePlugin({
-  $: "jquery",
-  jQuery: "jquery",
-  "window.jQuery": "jquery'",
-  "window.$": "jquery"
-});
-
-pluginArray.push(jq);
+pluginArray.push(new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  'window.jQuery': 'jquery',
+  'window.$': 'jquery'
+}));
 
 // Strip all locales except the ones defined in lib/language.js
 // (“en” is built into Moment and can’t be removed, 'dk' is not defined in moment)
-var momentLocales = new MomentLocalesPlugin({
+pluginArray.push(new MomentLocalesPlugin({
   localesToKeep: ['bg', 'cs', 'de', 'el', 'es', 'fi', 'fr', 'he', 'hr', 'it', 'ko', 'nb', 'nl', 'pl', 'pt', 'ro', 'ru',
     'sk', 'sv', 'zh_cn', 'zh_tw'],
-});
-pluginArray.push(momentLocales);
-
+}));
 
 module.exports = {
   context: path.resolve(__dirname, '.'),
@@ -75,14 +65,14 @@ module.exports = {
     path: path.resolve(__dirname, './tmp'),
     publicPath: '/',
     filename: 'js/bundle.js',
-    sourceMapFilename: "js/bundle.js.map",
+    sourceMapFilename: 'js/bundle.js.map',
   },
   devtool: sourceMapType,
   plugins: pluginArray,
   module: {
     rules: [{
       test: /\.(jpe?g|png|gif)$/i,
-      loader: "file-loader",
+      loader: 'file-loader',
       query: {
         name: '[name].[ext]',
         outputPath: 'images/'
@@ -93,9 +83,10 @@ module.exports = {
     },
       {
         test: /\.css$/,
-        loaders: ["style-loader", "css-loader"],
+        loaders: ['style-loader', 'css-loader'],
         exclude: /node_modules/
-      }, {
+      },
+      {
         test: require.resolve('jquery'),
         use: [{
           loader: 'expose-loader',
