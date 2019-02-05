5cd47d6377f05627624239da7dba2b06d2403a8d
PetrOndrusek <petr-ondrusek@seznam.cz>
2019-02-05 22:43:55 +0100

    testing Date header

--- tests/api3.security.test.js
+++ tests/api3.security.test.js
@@ -19,27 +19,57 @@ describe('Security of REST API3', function ( ) {
     });
   });
   
-  it('should require HTTPS', function (done) {
+  it('should require HTTPS', function () {
     request(self.http.baseUrl)
       .get('/api/v3/test')
       .expect(403)
       .end(function (err, res) {
         res.body.status.should.equal(403);
         res.body.message.should.equal(apiConst.MSG.HTTP_403_NOT_USING_HTTPS);
-
-        done();
       });
   });
 
-  it('should require Date header', function (done) {
+  it('should require Date header', function () {
     request(self.https.baseUrl)
       .get('/api/v3/test')
       .expect(401)
       .end(function (err, res) {
         res.body.status.should.equal(401);
         res.body.message.should.equal(apiConst.MSG.HTTP_401_MISSING_DATE);
+      });
+  });
 
-        done();
+  it('should validate Date header syntax', function () {
+    request(self.https.baseUrl)
+      .get('/api/v3/test')
+      .set('Date', 'invalid date header')
+      .expect(401)
+      .end(function (err, res) {
+        res.body.status.should.equal(401);
+        res.body.message.should.equal(apiConst.MSG.HTTP_401_BAD_DATE);
+      });
+  });
+
+  it('should reject Date header out of tolerance', function () {
+    var oldDate =  new Date((new Date() * 1) - 2 * 3600 * 1000);
+    var futureDate =  new Date((new Date() * 1) + 2 * 3600 * 1000);
+
+    request(self.https.baseUrl)
+      .get('/api/v3/test')
+      .set('Date',oldDate.toUTCString())
+      .expect(401)
+      .end(function (err, res) {
+        res.body.status.should.equal(401);
+        res.body.message.should.equal(apiConst.MSG.HTTP_401_DATE_OUT_OF_TOLERANCE);
+      });
+
+    request(self.https.baseUrl)
+      .get('/api/v3/test')
+      .set('Date',futureDate.toUTCString())
+      .expect(401)
+      .end(function (err, res) {
+        res.body.status.should.equal(401);
+        res.body.message.should.equal(apiConst.MSG.HTTP_401_DATE_OUT_OF_TOLERANCE);
       });
   });
 
