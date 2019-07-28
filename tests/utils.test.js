b6357f51e0adb9cbb293d3dd0cbe8aaddc79cf01
bassettb <bryanbassett@gmail.com>
2019-07-28 16:40:48 -0400

    Updated utils.toFixed

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -12,7 +12,16 @@ describe('utils', function ( ) {
   });
 
   it('format numbers', function () {
-    utils.toFixed(5.499999999).should.equal('5.50');
+    var undef;
+    utils.toFixed(3.345, 2).should.equal('3.35');
+    utils.toFixed(5.499999999, 0).should.equal('6');
+    utils.toFixed(5.499999999, 1).should.equal('5.5');
+    utils.toFixed(5.499999999, 3).should.equal('5.5');
+    utils.toFixed(123.45, -2).should.equal('100');
+
+    utils.toFixed(undef, 2).should.equal('0');
+    utils.toFixed(null, 2).should.equal('0');
+    utils.toFixed('text', 2).should.equal('0');
   });
 
   it('merge date and time', function () {
