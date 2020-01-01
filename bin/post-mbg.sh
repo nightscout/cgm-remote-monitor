3a1304ab5cc6e46b56d8e787ab286635229fe14b
malufukuyama <59417933+malufukuyama@users.noreply.github.com>
2020-01-01 17:47:35 -0300

    Update post-mbg.sh

--- bin/post-mbg.sh
+++ bin/post-mbg.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #  "date": "1413782506964"
 
-curl -H "Content-Type: application/json" -H "api-secret: $API_SECRET" -XPOST 'http://localhost:1337/api/v1/entries/' -d '{
+curl -H "Content-Type: application/json" -H "api-secret: $API_SECRET" -XPOST 'http://malufukxx1234@malufukuyama.herokuapp.com/api/v1/entries/' -d '{
   "mbg": 100,
   "type": "mbg",
   "date": "1413779270000"
