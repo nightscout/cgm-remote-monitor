9e103f7f6181ad64bb03d95cdc2c9fc7f2168733
malufukuyama <59417933+malufukuyama@users.noreply.github.com>
2020-01-01 17:48:19 -0300

    Update post-sgv.sh

--- bin/post-sgv.sh
+++ bin/post-sgv.sh
@@ -2,7 +2,7 @@
 # Date is epoch in nanosecods...ie linux echo $(($(date +%s%N)/1000000))
 # $API_SECRET needs to be a hashed value of your secret key...ie linux echo -n "<API_SECRET>" | sha1sum
 
-curl -H "Content-Type: application/json" -H "api-secret: $API_SECRET" -XPOST 'http://localhost:1337/api/v1/entries/' -d '{
+curl -H "Content-Type: application/json" -H "api-secret: $API_SECRET" -XPOST 'http://malufukxx1234@malufukuyama.herokuapp.com/api/v1/entries/' -d '{
   "sgv": 100,
   "type": "sgv",
   "direction": "Flat",
