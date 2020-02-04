e1623bda260e17f5d56b04e24f488a5f7dd20d59
Sulka Haro <sulka@sulka.net>
2020-02-04 09:41:28 +0200

    Code cleanup

--- views/service-worker.js
+++ views/service-worker.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var CACHE = '<%= locals.cachebuster %>';
 
 const CACHE_LIST = [
@@ -36,6 +38,35 @@ const CACHE_LIST = [
     '/images/logo2.png'
 ];
 
+// Open a cache and use `addAll()` with an array of assets to add all of them
+// to the cache. Return a promise resolving when all the assets are added.
+function precache() {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.addAll(CACHE_LIST);
+  });
+}
+
+// Open the cache where the assets were stored and search for the requested
+// resource. Notice that in case of no matching, the promise still resolves
+// but it does with `undefined` as value.
+function fromCache(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
+    });
+  });
+}
+
+// Update consists in opening the cache, performing a network request and
+// storing the new response data.
+function update(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response);
+    });
+  });
+}
+
 // On install, cache some resources.
 self.addEventListener('install', function(evt) {
   console.log('The service worker is being installed.');
@@ -59,59 +90,15 @@ function inCache(request) {
 self.addEventListener('fetch', function(evt) {
 
   if (!evt.request.url.startsWith(self.location.origin) || CACHE == 'developmentMode' || !inCache(evt.request) || evt.request.method !== 'GET') {
-    // External request, or POST, ignore
     console.log('Skipping cache for ',  evt.request.url);
     return void evt.respondWith(fetch(evt.request));
   }
 
   console.log('Returning cached for ',  evt.request.url);
 
-  // You can use `respondWith()` to answer immediately, without waiting for the
-  // network response to reach the service worker...
   evt.respondWith(fromCache(evt.request));
-  // ...and `waitUntil()` to prevent the worker from being killed until the
-  // cache is updated.
   evt.waitUntil(update(evt.request));
 });
-
-// Open a cache and use `addAll()` with an array of assets to add all of them
-// to the cache. Return a promise resolving when all the assets are added.
-function precache() {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.addAll(CACHE_LIST);
-  });
-}
-
-// Open the cache where the assets were stored and search for the requested
-// resource. Notice that in case of no matching, the promise still resolves
-// but it does with `undefined` as value.
-function fromCache(request) {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.match(request).then(function (matching) {
-      return matching || Promise.reject('no-match');; //|| fromNetwork(request, 10000);
-    });
-  });
-}
-
-// Update consists in opening the cache, performing a network request and
-// storing the new response data.
-function update(request) {
-  return caches.open(CACHE).then(function (cache) {
-    return fetch(request).then(function (response) {
-      return cache.put(request, response);
-    });
-  });
-}
-
-function fromNetwork(request, timeout) {
-    return new Promise(function (fulfill, reject) {
-      var timeoutId = setTimeout(reject, timeout);
-       fetch(request).then(function (response) {
-        clearTimeout(timeoutId);
-        fulfill(response);
-      }, reject);
-    });
-  }
 
 self.addEventListener('activate', event => {
   event.waitUntil(
