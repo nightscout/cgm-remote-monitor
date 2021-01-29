480ae3393f25365d189595acf8457248b4085189
Matt Gaide <mgaide@salesforce.com>
2021-01-29 12:31:21 -0800

    #6701 Test cleanup

--- tests/reportstorage.test.js
+++ tests/reportstorage.test.js
@@ -1,7 +1,35 @@
 const should = require('should');
 const reportstorage = require('../lib/report/reportstorage');
+let storage = require('js-storage').localStorage;
+let mockStorage = require('./fixtures/localstorage');
+const defaultValues = {
+    insulin: true,
+    carbs: true,
+    basal: true,
+    notes: false,
+    food: true,
+    raw: false,
+    iob: false,
+    cob: false,
+    predicted: false,
+    openAps: false,
+    insulindistribution: true,
+    predictedTruncate: true
+};
 
 describe('reportstorage unit tests', () => {
+
+    beforeEach(() => {
+        storage._get = storage.get;
+        storage._set = storage.set;
+        storage.get = mockStorage.get;
+        storage.set = mockStorage.set;
+    });
+
+    afterEach(() => {
+        storage.get = storage._get;
+        storage.set = storage._set;
+    });
 
     it('reportstorage definition - returns saveProps and getValue function', () => {
         reportstorage.should.not.be.undefined();
@@ -10,20 +38,6 @@ describe('reportstorage unit tests', () => {
     });
 
     it('reportstorage.getValue returns default properties', () => {
-        const defaultValues = {
-            insulin: true,
-            carbs: true,
-            basal: true,
-            notes: false,
-            food: true,
-            raw: false,
-            iob: false,
-            cob: false,
-            predicted: false,
-            openAps: false,
-            insulindistribution: true,
-            predictedTruncate: true
-        };
         let keyCount = 0;
         for (const v in defaultValues) {
             reportstorage.getValue(v).should.equal(defaultValues[v]);
@@ -33,20 +47,12 @@ describe('reportstorage unit tests', () => {
     });
 
     it('reportstorage.saveProps sets property in localstorage', () => {
-        const storage = require('js-storage').localStorage;
-        const mockStorage = require('./fixtures/localstorage');
-        storage.get = mockStorage.get;
-        storage.set = mockStorage.set;
         reportstorage.saveProps({insulin: false});
         should.exist(mockStorage.get('reportProperties'));
         mockStorage.get('reportProperties').insulin.should.be.false();
     });
 
     it('reportstorage.saveProps ignores property not tracked', () => {
-        const storage = require('js-storage').localStorage;
-        const mockStorage = require('./fixtures/localstorage');
-        storage.get = mockStorage.get;
-        storage.set = mockStorage.set;
         reportstorage.saveProps({foo: 'bar'});
         should.exist(mockStorage.get('reportProperties'));
         should.not.exist(mockStorage.get('reportProperties').foo);
