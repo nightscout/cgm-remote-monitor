nightscout/cgm-remote-monitor

commit aca8d5ab492f1e356c36f340102044dc961d82aa

Fixes an issue where treatment units were all rewritten on load, regardless of units (#7273)

lib/data/ddata.js
@@ -265,7 +265,11 @@ function init () {
     ddata.tempbasalTreatments = ddata.processDurations(tempbasalTreatments, false);
 
     // filter temp target
-    var tempTargetTreatments = ddata.treatments.filter(function filterTargets (t) {
+    var tempTargetTreatments = ddata.treatments.filter(function filterTargets (tt) {
+
+      // Clone the treatment before modifying it
+      let t = _.cloneDeep(tt);
+
       //check for a units being sent
       if (t.units) {
         if (t.units == 'mmol') {

commit 4d8171678be8f5bb98e7d6fb7c6fd32e88eb3944

Fixes a bug introduced in #7273

lib/data/ddata.js
@@ -266,9 +266,10 @@ function init () {
 
     // filter temp target
     var tempTargetTreatments = ddata.treatments.filter(function filterTargets (tt) {
+      return t.eventType && t.eventType.indexOf('Temporary Target') > -1;
+    });
 
-      // Clone the treatment before modifying it
-      let t = _.cloneDeep(tt);
+    function fixUnits (t) {
 
       //check for a units being sent
       if (t.units) {
@@ -279,19 +280,22 @@ function init () {
           t.units = 'mg/dl';
         }
       }
+      
       //if we have a temp target thats below 20, assume its mmol and convert to mgdl for safety.
-      if (t.targetTop < 20) {
+      if (t.targetTop < 20 || t.targetBottom < 20) {
         t.targetTop = t.targetTop * consts.MMOL_TO_MGDL;
-        t.units = 'mg/dl';
-      }
-      if (t.targetBottom < 20) {
         t.targetBottom = t.targetBottom * consts.MMOL_TO_MGDL;
         t.units = 'mg/dl';
       }
-      return t.eventType && t.eventType.indexOf('Temporary Target') > -1;
-    });
+    }
+
     if (preserveOrignalTreatments)
       tempTargetTreatments = _.cloneDeep(tempTargetTreatments);
+    
+    for (let i = 0; i <= tempTargetTreatments.length; i++) {
+      fixUnits(tempTargetTreatments[i]);
+    }
+
     ddata.tempTargetTreatments = ddata.processDurations(tempTargetTreatments, false);
 
   };