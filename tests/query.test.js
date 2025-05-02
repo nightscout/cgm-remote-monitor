9d5a183b729bba80376b6a522113513b6be5d702
Rhys <36913739+ryceg@users.noreply.github.com>
2025-05-02 16:03:29 +1000

    all 1:1 conversions

--- tests/query.test.js
+++ tests/query.test.js
@@ -2,7 +2,7 @@
 
 require('should');
 
-var moment = require('moment');
+var dayjs = require('dayjs');
 
 describe('query', function ( ) {
   var query = require('../lib/server/query');
@@ -10,8 +10,8 @@ describe('query', function ( ) {
   it('should provide default options', function ( ) {
     var opts = query();
 
-    var low = moment().utc().subtract(4, 'days').subtract(1, 'minutes').format();
-    var high = moment().utc().subtract(4, 'days').add(1, 'minutes').format();
+    var low = dayjs().utc().subtract(4, 'days').subtract(1, 'minutes').format();
+    var high = dayjs().utc().subtract(4, 'days').add(1, 'minutes').format();
 
     opts.date['$gte'].should.be.greaterThan(low);
     opts.date['$gte'].should.be.lessThan(high);
@@ -23,8 +23,8 @@ describe('query', function ( ) {
       dateField: 'created_at'
     });
 
-    var low = moment().utc().subtract(2, 'days').subtract(1, 'minutes').format();
-    var high = moment().utc().subtract(2, 'days').add(1, 'minutes').format();
+    var low = dayjs().utc().subtract(2, 'days').subtract(1, 'minutes').format();
+    var high = dayjs().utc().subtract(2, 'days').add(1, 'minutes').format();
 
     opts.created_at['$gte'].should.greaterThan(low);
     opts.created_at['$gte'].should.lessThan(high);
