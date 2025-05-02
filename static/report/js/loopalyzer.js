b2a529ebc4246a260dfba099a5e410fbb6332269
Rhys <36913739+ryceg@users.noreply.github.com>
2025-05-02 16:41:45 +1000

    rename moment in ctx

--- static/report/js/loopalyzer.js
+++ static/report/js/loopalyzer.js
@@ -1,8 +1,8 @@
 // Moves backward one day
 function loopalyzerBackward() {
-    var moment = window.moment;
-    var from = moment($("#rp_from").val()).subtract(1,'day');
-    var to = moment($("#rp_to").val()).subtract(1,'day');
+    var dayjs = window.dayjs;
+    var from = dayjs($("#rp_from").val()).subtract(1,'day');
+    var to = dayjs($("#rp_to").val()).subtract(1,'day');
     $("#rp_from").val(from.format('YYYY-MM-DD'));
     $("#rp_to").val(to.format('YYYY-MM-DD'));
     $("#rp_show").click();
@@ -10,9 +10,9 @@ function loopalyzerBackward() {
 
 // Moves backward same amount as shown (e.g. whole week)
 function loopalyzerMoreBackward() {
-    var moment = window.moment;
-    var from = moment($("#rp_from").val())
-    var to = moment($("#rp_to").val())
+    var dayjs = window.dayjs;
+    var from = dayjs($("#rp_from").val())
+    var to = dayjs($("#rp_to").val())
     var diff = to.diff(from, 'days') + 1;
     from.subtract(diff, 'days');
     to.subtract(diff, 'days');
@@ -23,10 +23,10 @@ function loopalyzerMoreBackward() {
 
 // Moves forward one day
 function loopalyzerForward() {
-    var moment = window.moment;
-    var from = moment($("#rp_from").val()).add(1,'day');
-    var to = moment($("#rp_to").val()).add(1,'day');
-    if (to <= moment()) {
+    var dayjs = window.dayjs;
+    var from = dayjs($("#rp_from").val()).add(1,'day');
+    var to = dayjs($("#rp_to").val()).add(1,'day');
+    if (to <= dayjs()) {
         $("#rp_from").val(from.format('YYYY-MM-DD'));
         $("#rp_to").val(to.format('YYYY-MM-DD'));
         $("#rp_show").click();
@@ -35,15 +35,15 @@ function loopalyzerForward() {
 
 // Moves forward same amount as shown (e.g. whole week)
 function loopalyzerMoreForward() {
-    var moment = window.moment;
-    var from = moment($("#rp_from").val())
-    var to = moment($("#rp_to").val())
+    var dayjs = window.dayjs;
+    var from = dayjs($("#rp_from").val())
+    var to = dayjs($("#rp_to").val())
     var diff = to.diff(from, 'days') + 1;
     from.add(diff, 'days');
     to.add(diff, 'days');
-    if (to > moment()) {
-        to = moment();
-        from = moment();
+    if (to > dayjs()) {
+        to = dayjs();
+        from = dayjs();
         from.subtract(diff-1, 'days');
     }
     $("#rp_from").val(from.format('YYYY-MM-DD'));
